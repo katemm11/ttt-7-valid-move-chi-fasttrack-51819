f5fd0b80f8072a98f32283b77744760e65a5d7ca
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 22:10:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index < board.length && position_taken? == false
+  if index < board.length && position_taken?(board, index) == false
   return true
   elsif index > board.length
   return false
