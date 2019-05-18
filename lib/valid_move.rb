b59aa7555c8441a0168016cae730a7a092af793b
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 22:02:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if index < board.length && position_taken? == false
+  return true
+  elsif index > board.length
+  return false
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  index.to_i
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
+  end
+  end
